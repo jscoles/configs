2445f40a2a90c55df583b9896c301eca9b6aecd2
Jonathan Scoles <jscoles@meraki.com>
2015-05-20 09:37:35 -0700

    update tmux, remove vimbundle updater

--- vim/update-vim-bundles-list.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-CONFIG_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-BUNDLE_DIR="$HOME/.vim/bundle"
-ls -lrt -d -1 $BUNDLE_DIR/* | awk '{print $9}' | xargs -I {} -i zsh -c "cd {} && git remote -v | grep '(push)' | grep 'git://' | awk '{print \$2}'" | tee "$CONFIG_DIR/vim-bundles.txt"
-
